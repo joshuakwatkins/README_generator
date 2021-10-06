7cb8c2600343c81fa6676442d2383f6ed832fad7
Josh Watkins <joshwatkinssfs@gmail.com>
2021-10-06 17:36:11 -0400

    saving my progress

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js")
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "gitUser",
+        message: "What is your GitHub user name?"
+    },
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "descrip",
+        message: "A short description of your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license should be associated with this project?",
+        choices: [
+            "MIT",
+            "APACHE 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What command should we use to install the necessary dependencies?"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Let's build your README.md!")
+    inquirer.prompt(questions)
+    .then((result) => {
+        console.log(result)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,84 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+let badgeURL = "";
+let licenseURL = "";
+let licenseSection = ""
+function renderLicenseBadge(data) {
+  switch (data.license) {
+    case "MIT":
+      badgeURL = "https://img.shields.io/badge/license-MIT-international.svg";
+      break;
+    case "APACHE 2.0":
+      badgeURL = "https://img.shields.io/badge/license-APACHE%202.0-international.svg";
+      break;
+    case "GPL 3.0":
+      badgeURL = "https://img.shields.io/badge/license-GPL%203.0-international.svg";
+      break;
+    case "BSD 3":
+      badgeURL = "https://img.shields.io/badge/license-BSD%203-international.svg";
+      break;
+    default: badgeUrl = "";
+  }
+  return badgeURL;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  switch (data.license) {
+    case "MIT":
+      licenseURL = "https://opensource.org/licenses/MIT";
+      break;
+    case "APACHE 2.0":
+      licenseURL = "https://opensource.org/licenses/Apache-2.0";
+      break;
+    case "GPL 3.0":
+      licenseURL = "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+    case "BSD 3":
+      licenseURL = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+    default: licenseUrl = "";
+  }
+  return licenseURL
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  if (data.license !== "None"){
+    licenseSection = `![GitHub License](${renderLicenseBadge(data)})](${renderLicenseLink(data)})`
+  } else {
+    licenseSection = '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseSection(data)}
+
+  ## Description
+
+  ${data.descrip}
+
+  ## Table Of Contents
+*[Installation](#!installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+
+## Installation
+
+To install the necessary dependencies, run the following command:
+
+\`\`\`
+${data.install}
+\`\`\`
+
 
 `;
 }
